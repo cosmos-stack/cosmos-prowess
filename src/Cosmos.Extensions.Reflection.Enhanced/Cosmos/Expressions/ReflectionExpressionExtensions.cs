5f635f2e65aaeb0d25d77df428dd730d6f9c0728
alexinea <forerunner@outlook.com>
2021-07-11 11:38:21 +0800

    Update reflection compoments.

---
+++ src/Cosmos.Extensions.Reflection.Enhanced/Cosmos/Expressions/ReflectionExpressionExtensions.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using Cosmos.Reflection;
+
+namespace Cosmos.Expressions
+{
+    public static class ReflectionExpressionExtensions
+    {
+        #region GetMethod
+
+        public static MethodInfo GetMethod<T>(this Expression<T> expression)
+        {
+            if (expression is null)
+                throw new ArgumentNullException(nameof(expression));
+
+            if (expression.Body is not MethodCallExpression methodCallExpression)
+                throw new InvalidCastException("Cannot be converted to MethodCallExpression");
+
+            return methodCallExpression.Method;
+        }
+
+        public static MethodCallExpression GetMethodExpression<T>(this Expression<Action<T>> method)
+        {
+            if (method.Body.NodeType != ExpressionType.Call)
+                throw new ArgumentException("Method call expected", method.Body.ToString());
+            return (MethodCallExpression)method.Body;
+        }
+
+        public static MethodCallExpression GetMethodExpression<T>(this Expression<Func<T, object>> exp)
+        {
+            switch (exp.Body.NodeType)
+            {
+                case ExpressionType.Call:
+                    return (MethodCallExpression)exp.Body;
+
+                case ExpressionType.Convert:
+                    if (exp.Body is UnaryExpression { Operand: MethodCallExpression methodCallExpression })
+                    {
+                        return methodCallExpression;
+                    }
+
+                    break;
+            }
+
+            throw new InvalidOperationException($"Method expected: {exp.Body}");
+        }
+
+        #endregion
+
+        #region GetMember
+
+        /// <summary>
+        /// GetMemberName
+        /// </summary>
+        /// <typeparam name="TEntity">TEntity</typeparam>
+        /// <typeparam name="TMember">TMember</typeparam>
+        /// <param name="memberExpression">get member expression</param>
+        /// <returns></returns>
+        public static string GetMemberName<TEntity, TMember>(this Expression<Func<TEntity, TMember>> memberExpression)
+        {
+            return GetMemberInfo(memberExpression).Name;
+        }
+
+        /// <summary>
+        /// GetMemberInfo
+        /// </summary>
+        /// <typeparam name="TEntity">TEntity</typeparam>
+        /// <typeparam name="TMember">TMember</typeparam>
+        /// <param name="expression">get member expression</param>
+        /// <returns></returns>
+        public static MemberInfo GetMemberInfo<TEntity, TMember>(this Expression<Func<TEntity, TMember>> expression)
+        {
+            if (expression.NodeType != ExpressionType.Lambda)
+                throw new ArgumentException($"{nameof(expression)} must be Lambda Expression", nameof(expression));
+
+            var lambda = (LambdaExpression)expression;
+
+            var memberExpression = ExtractMemberExpression(lambda.Body);
+            if (memberExpression is null)
+                throw new ArgumentException($"{nameof(expression)} must be Lambda Expression", nameof(expression));
+
+            return memberExpression.Member;
+        }
+
+        private static MemberExpression ExtractMemberExpression(Expression expression)
+        {
+            if (expression.NodeType == ExpressionType.MemberAccess)
+                return (MemberExpression)expression;
+
+            if (expression.NodeType == ExpressionType.Convert)
+                return ExtractMemberExpression(((UnaryExpression)expression).Operand);
+
+            throw new InvalidOperationException(nameof(ExtractMemberExpression));
+        }
+
+        #endregion
+
+        #region GetProperty
+
+        /// <summary>
+        /// GetPropertyInfo
+        /// </summary>
+        /// <typeparam name="TEntity"></typeparam>
+        /// <typeparam name="TProperty"></typeparam>
+        /// <param name="expression"></param>
+        /// <returns></returns>
+        public static PropertyInfo GetProperty<TEntity, TProperty>(this Expression<Func<TEntity, TProperty>> expression)
+        {
+            var member = GetMemberInfo(expression);
+
+            if (null == member)
+                throw new InvalidOperationException("no property found");
+
+            if (member is PropertyInfo property)
+                return property;
+
+            return TypeReflections.TypeCacheManager.GetTypeProperties(typeof(TEntity)).First(p => p.Name.Equals(member.Name));
+        }
+
+        #endregion
+    }
+}
---
+++ src/Cosmos.Extensions.Reflection.Enhanced/Cosmos/Reflection/Assignment/AssignmentExtensions.cs
@@ -0,0 +1,37 @@
+﻿using System.Reflection;
+
+namespace Cosmos.Reflection.Assignment
+{
+    public static class AssignmentExtensions
+    {
+        public static object GetValue<T>(this T t, string memberName)
+        {
+            return t.GetValueAccessor().GetValue(memberName);
+        }
+
+        public static object GetValue<T>(this T t, string memberName, BindingFlags bindingAttr)
+        {
+            return t.GetValueAccessor().GetValue(memberName, bindingAttr);
+        }
+
+        public static TM GetValue<T, TM>(this T t, string memberName)
+        {
+            return (TM)t.GetValueAccessor().GetValue(memberName);
+        }
+
+        public static TM GetValue<T, TM>(this T t, string memberName, BindingFlags bindingAttr)
+        {
+            return (TM)t.GetValueAccessor().GetValue(memberName, bindingAttr);
+        }
+        
+        public static void SetValue<T>(this T t, string memberName, object value)
+        {
+            t.GetValueAccessor().SetValue(memberName, value);
+        }
+
+        public static void SetValue<T>(this T t, string memberName, object value, BindingFlags bindingAttr)
+        {
+            t.GetValueAccessor().SetValue(memberName, bindingAttr, value);
+        }
+    }
+}
