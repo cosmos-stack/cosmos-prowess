da0f8a835d517d89348c99d559c2268c97d7cd3a
AlexLEWIS <forerunner@outlook.com>
2022-12-08 17:03:38 +0800

    Update UT

---
+++ test/CosmosProwessUT/TextUT/StringTruncateTests.cs
@@ -0,0 +1,20 @@
+﻿using Cosmos.Text;
+
+namespace CosmosProwessUT.TextUT;
+
+[Trait("StringUT", "Strings.Additional")]
+public class StringTruncateTests
+{
+    [Fact(DisplayName = "Truncate test")]
+    public void TruncateTest()
+    {
+        var str = "Nietooo";
+
+        str.Truncate(8).ShouldBe("Nietooo");
+        str.Truncate(7).ShouldBe("Nietooo");
+        str.Truncate(6).ShouldBe("Nie...");
+        str.Truncate(5).ShouldBe("Ni...");
+        str.Truncate(4).ShouldBe("N...");
+        str.Truncate(3).ShouldBe("Ni.");
+    }
+}
---
+++ test/CosmosProwessUT/TextUT/StringWordsTests.cs
@@ -0,0 +1,46 @@
+﻿using System.Linq;
+using Cosmos.Text;
+
+namespace CosmosProwessUT.TextUT;
+
+[Trait("StringUT", "Strings.Words")]
+public class StringWordsTests
+{
+    [Fact(DisplayName = "String/Words CapitalCase test")]
+    public void CapitalCaseTest()
+    {
+        var text = "nice to meet you, AlexLEWIS";
+        StringWords.ToCapitalCase(text).ShouldBe("Nice To Meet You, Alexlewis");
+    }
+
+    [Fact(DisplayName = "String/Words CamelCase test")]
+    public void CamelCaseTest()
+    {
+        var text = "AlexLEWIS DuDuDu";
+        StringWords.ToCapitalCase(text).ShouldBe("Alexlewis Dududu");
+    }
+
+    [Fact(DisplayName = "String/Words CountByWords test")]
+    public void CountByWordsTest()
+    {
+        var text = "Nice to meet you";
+        StringWords.CountByWords(text).ShouldBe(4);
+    }
+
+    [Fact(DisplayName = "String/Words SplitByWords test")]
+    public void SplitByWordsTest()
+    {
+        var text = "Nice to meet you";
+        var vals = StringWords.SplitByWords(text).ToList();
+
+        vals.Count.ShouldBe(4);
+    }
+
+    [Fact(DisplayName = "String/Words TruncateByWords test")]
+    public void TruncateByWordsTest()
+    {
+        var text = "Nice to meet you";
+        StringWords.TruncateByWords(text, 3).ShouldBe("Nice to meet...");
+        StringWords.TruncateByWords(text, 3, extraSpace: true).ShouldBe("Nice to meet ...");
+    }
+}
